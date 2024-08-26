6d5866e79905dace3207d4b00450cade0623f917
Andrew <me@andrewvacca.ro>
2024-08-26 08:55:48 -0400

    wip

--- page-contracts.php
+++ page-contracts.php
@@ -12,26 +12,61 @@ get_header()
     ?>
 
 <section class="py-20">
-    <div class="container">
+    <div class="container grid grid-cols-3 gap-8">
         <?php $item = get_post(61); ?>
-        <ul>
-            <li>
-                <a target="_blank" class="flex items-center px-4 py-4 space-x-4 transition-colors hover:text-white bg-gray-50 hover:bg-green group " href="<?= get_permalink($item); ?>">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                </svg>
-                    <span class="flex-grow"><?= get_the_title($item) ?></span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="ml-0 size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                </svg>
-                </a>
+        <div>
+            <h3 class="mb-6 text-3xl text-green">Service Center</h3>
+            <ul>
+                <li>
+                    <a target="_blank"
+                        class="flex items-center px-4 py-4 space-x-4 transition-colors hover:text-white bg-gray-50 hover:bg-green group "
+                        href="<?= get_permalink($item); ?>">
+                        <span class="flex-grow"><?= get_the_title($item) ?></span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="ml-0 size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                        </svg>
+                    </a>
 
-            </li>
-        </ul>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="mb-6 text-3xl text-green">Food Service Workers</h3>
+            <ul>
+                <li>
+                    <a target="_blank"
+                        class="flex items-center px-4 py-4 space-x-4 transition-colors hover:text-white bg-gray-50 hover:bg-green group "
+                        href="<?= get_permalink($item); ?>">
+                        <span class="flex-grow"><?= get_the_title($item) ?></span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="ml-0 size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                        </svg>
+                    </a>
+
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="mb-6 text-3xl text-green">Cook Managers</h3>
+            <ul>
+                <li>
+                    <a target="_blank"
+                        class="flex items-center px-4 py-4 space-x-4 transition-colors hover:text-white bg-gray-50 hover:bg-green group "
+                        href="<?= get_permalink($item); ?>">
+                        <span class="flex-grow"><?= get_the_title($item) ?></span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="ml-0 size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
 </section>
 
